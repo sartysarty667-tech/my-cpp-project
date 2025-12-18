4f44f4fc55fb4a508fab45cbab0e1eeb3a2a0869
Galymzhan <sartysarty667@gmail.com>
2025-12-18 21:03:38 +0600

    First commit: C++ algorithm tasks

---
+++ 5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+int main()
+{
+    string s ,t;
+    cin >> s >> t;
+
+    map<char ,int> v;
+
+    for( char c : s){
+        v[c]++;
+    }
+
+    for(char c : t){
+        v[c]--;
+    }
+
+    for(pair<char ,int> w : v){
+       if(w.second != 0) {
+        cout << "No";
+        return 0;
+       }
+    }
+
+    cout << "Yes";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
